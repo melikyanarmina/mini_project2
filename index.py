82e04ffb30006722f5072e78cac56cce3c6eaabe
melikyanarmina <158816882+melikyanarmina@users.noreply.github.com>
2024-02-15 17:38:18 +0400

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,29 @@
-import random
-dice_a= random.randint(1, 6)
-dice_b= random.randint(1, 6)
+def roll_dice():
+    return random.randint(1, 6) + random.randint(1, 6)
 
-first_roll=dice_a+dice_b
+
+dice_a = random.randint(1, 6)
+dice_b = random.randint(1, 6)
+first_roll = dice_a + dice_b
 print(f"First roll: {first_roll}")
 
 if first_roll == 7 or first_roll == 11:
-   print("You win")
-elif first_roll in (2,3,12):
-   print("Casino wins")
-goal_nums=4,5,6,8,9,10
+    print("You win")
+elif first_roll in (2, 3, 12):
+    print("Casino wins")
+else:
+    print(f"Goal number is: {first_roll}")
 
-print(f"Goal numbers are: {goal_nums}")
-next_roll = dice_a + dice_b
+    goal_number = first_roll
 
-
-if next_roll == 7:
+    while True:
+        next_roll = roll_dice()
+        print(f"Next roll: {next_roll}")
+        if next_roll == 7:
             print("You lose")
-
-elif next_roll in goal_nums:
+            break
+        elif next_roll == goal_number:
             print("Player wins")
-
+            break
+        else:
+            print("Roll again")
